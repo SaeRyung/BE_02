08e69ddb1a232ca52d17914f8202392a4eedf42c
SaeRyung <101532588+SaeRyung@users.noreply.github.com>
2024-06-08 01:59:22 +0900

    반복문_트리구현, 수정필요

---
+++ src/BE_cordingtest/w2/for_Diamond.java
@@ -0,0 +1,38 @@
+package BE_cordingtest.w2;
+
+public class for_Diamond {
+    public static void main(String[] args) {
+        //트리 윗부분
+        for(int i = 0; i<5; i++) {
+
+            for(int j=5; j>i; j--){
+                System.out.print(" ");
+            }
+            for (int j = 0; j<=i; j++) {
+                System.out.print("*");
+            }
+            for (int j = 0; j<i; j++) {
+                System.out.print("*");
+            }
+
+
+            System.out.println();
+        }
+
+        //트리 아랫부분
+        for(int i = 0; i<5; i++) {
+            for(int j=0; j<i; j++){
+                System.out.print(" ");
+            }
+            for(int j=5; j>=i; j--){
+                System.out.print("*");
+            }
+            for(int j=5; j>i; j--){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+    }
+}
